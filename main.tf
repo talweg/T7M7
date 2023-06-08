66d0c9db5d76dc416257aff052a1db9a86d2c848
Serhii <talweg.ua@gmail.com>
2023-06-08 16:32:49 +0300

    modify main

--- main.tf
+++ main.tf
@@ -1,22 +1,3 @@
-resource "google_container_cluster" "demo" {
-  name     = "demo"
-  location = var.GOOGLE_REGION
-
-  remove_default_node_pool = true
-  initial_node_count       = 1
-}
-
-resource "google_container_node_pool" "main" {
-  name       = "main"
-  project    = google_container_cluster.demo.project
-  cluster    = google_container_cluster.demo.name
-  location   = google_container_cluster.demo.location
-  node_count = var.GKE_NUM_NODES
-
-  node_config {
-    machine_type = var.GKE_MACHINE_TYPE
-  }
-}
 module "gke_cluster" {
   source         = "github.com/talweg/tf-google-gke-cluster"
   GOOGLE_REGION  = var.GOOGLE_REGION
